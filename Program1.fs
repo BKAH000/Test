36fbf8d9c4963e1724c9fdec056c2d4dad966a8b
BKAH000 <31971024+BKAH000@users.noreply.github.com>
2017-09-14 22:17:33 +0200

    Add files via upload

--- Program1.fs
+++ Program1.fs
@@ -9,4 +9,5 @@ let rec fibonacci2 (steps:int, fib_list:List<int>):List<int> =
     | _ -> fibonacci2(steps-1, List.append fib_list [fib_list.[fib_list.Length-1] + fib_list.[fib_list.Length-2]])
 let main = fibonacci2(4, [1;1]) |> printfn "%A"
 main
+//Kommentar
 Console.ReadKey() |> ignore                                              
